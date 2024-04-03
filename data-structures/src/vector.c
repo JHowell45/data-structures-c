a790a9434914e0f2f0a50c08f936608d0d6f13ba
Jacob <jacob@notactivelylooking.com>
2024-04-03 13:10:09 +0100

    define creating a new vector, freeing it, and printing it.

--- data-structures/src/vector.c
+++ data-structures/src/vector.c
@@ -1,3 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "vector.h"
+
+#define VECTOR_CAPACITY 4
+
+vector * newVector(void) {
+    vector *v = malloc(sizeof(vector));
+    v->capacity = VECTOR_CAPACITY;
+    v->size = 0;
+    v->items = calloc(VECTOR_CAPACITY, sizeof(int));
+    return v;
+}
+
+void freeVector(vector *vector) {
+    free(vector->items);
+    free(vector);
+}
+
+void printVector(vector *vector) {
+    printf("[");
+    for (int i = 0; i < vector->size; i++) {
+        int item = vector->items[i];
+        if (i == 0) {
+            printf("%d", item);
+        } else {
+            printf(", %d", item);
+        }
+    }
+    printf("]\n");
+}

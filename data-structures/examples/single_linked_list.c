22a3a1f01f5c642c8ee65bf28bcec907198bfa9f
Jacob <jacob@notactivelylooking.com>
2024-04-05 20:18:32 +0100

    tester.

--- data-structures/examples/single_linked_list.c
+++ data-structures/examples/single_linked_list.c
@@ -1,12 +1,27 @@
+#include <stdio.h>
 #include "single_linked_list.h"
 
 int main(void) {
-    LinkedList *ll = newSingleLL(0);
-    printSingleLL(ll);
+    LinkedList *root = newSingleLinkedList(0);
+    printSingleLinkedList(root);
 
-    for (int i = 1; i <= 5; i++) {
-        pushSingleLL(ll, i);
-        printSingleLL(ll);
+    for (int i = 1; i <= 10; i++) {
+        pushSingleLinkedList(root, i);
+        printSingleLinkedList(root);
     }
+
+    insertSingleLinkedList(root, 1000, 4);
+    printSingleLinkedList(root);
+
+    removeSingleLinkedList(root, 5);
+    printSingleLinkedList(root);
+
+    size_t superLargeValue = 1000000;
+    for (int i = 0; i < superLargeValue; i++) {
+        insertSingleLinkedList(root, i, 6);
+        printf("Current Iter: %d\n", i);
+    }
+
+    freeSingleLinkedList(root);
     return 0;
 }

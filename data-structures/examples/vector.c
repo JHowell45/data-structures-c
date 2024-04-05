40609b491a4e650fb036709c2a83e580e65dd342
Jacob <jacob@notactivelylooking.com>
2024-04-05 13:15:39 +0100

    testing new methods.

--- data-structures/examples/vector.c
+++ data-structures/examples/vector.c
@@ -2,7 +2,20 @@
 
 int main(void) {
     vector *v = newVector();
-    printVector(v);
+    printVerboseVector(v);
+    for (int i = 0; i < 20; i++) {
+        pushVector(v, i);
+    }
+    printVerboseVector(v);
+    for (int i = 100; i > 80; i--) {
+        insertVector(v, 100, 5);
+        printVerboseVector(v);
+    }
+    for (int i = 0; i < 30; i++) {
+        popVector(v);
+        printVerboseVector(v);
+    }
+    printVerboseVector(v);
     freeVector(v);
     return 0;
 }

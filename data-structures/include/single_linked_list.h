b2f1f93a678196db4e8dd991be939860a57dabd2
Jacob <jacob@notactivelylooking.com>
2024-08-23 18:42:21 +0100

    new struct specifically for the linked list that tracks the head, tail, and length.

--- data-structures/include/single_linked_list.h
+++ data-structures/include/single_linked_list.h
@@ -19,4 +19,14 @@ void single_linked_list_node_pop(linked_list_node_t* ll);
 void single_linked_list_node_remove(linked_list_node_t* ll, size_t index);
 linked_list_node_t* single_linked_list_node_get_node(linked_list_node_t* ll, int searchValue);
 
+typedef struct single_linked_list {
+    linked_list_node_t* head;
+    linked_list_node_t* tail;
+    size_t length;
+} single_linked_list_t;
+
+single_linked_list_t* new_single_linked_list(void);
+
+void free_single_linked_list(single_linked_list_t* list);
+
 #endif

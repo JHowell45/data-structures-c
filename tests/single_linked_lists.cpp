be1387917903f8390b0a67383ff49cd3ce209f50
Jacob <jacob@notactivelylooking.com>
2024-08-25 23:08:15 +0100

    Setting up basic fixture and tests.

--- tests/single_linked_lists.cpp
+++ tests/single_linked_lists.cpp
@@ -2,10 +2,23 @@
 
 #include "single_linked_list.h"
 
+class SingleLinkedListTest : public testing::Test {
+  protected:
+    linked_list_node_t* list;
+
+    void
+    SetUp() {
+        list = new_single_linked_list_node(0);
+    }
+
+    void
+    TearDown() {
+        free_single_linked_list_node(list);
+    }
+};
+
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
+TEST_F(SingleLinkedListTest, test_push) {
+    single_linked_list_node_push(list, 1);
+    EXPECT_EQ(list->data, 0);
 }

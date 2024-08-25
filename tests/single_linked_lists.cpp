1ac628c72a5f79ff4b2a5fcf2a07435a930692e1
Jacob <jacob@notactivelylooking.com>
2024-08-26 00:07:35 +0100

    More tests.

--- tests/single_linked_lists.cpp
+++ tests/single_linked_lists.cpp
@@ -20,7 +20,10 @@ class SingleLinkedListTest : public testing::Test {
 };
 
 // Demonstrate some basic assertions.
+TEST_F(SingleLinkedListTest, test_root) { EXPECT_EQ(list->data, 0); }
+
+// Demonstrate some basic assertions.
 TEST_F(SingleLinkedListTest, test_push) {
     single_linked_list_node_push(list, 1);
-    EXPECT_EQ(list->data, 0);
+    EXPECT_EQ(list->next->data, 1);
 }
